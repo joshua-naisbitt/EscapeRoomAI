b0b3395684eff1596e6090b555b09e43d8d54c23
Josh <74276429+joshua-naisbitt@users.noreply.github.com>
2024-11-14 22:27:35 -0600

    wip: manually adding inventory system features progress

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,7 +1,3 @@
-/*
-Josh, Wrote entire class
-*/
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,6 +10,8 @@ public class InputManager : MonoBehaviour // MonoBehavior is a base class in Uni
     public PlayerInput.OnFootActions onFoot;
     private PlayerMotor motor;
     private PlayerLook look;
+    private GameObject inventoryCanvas;
+    private bool isInventoryVisible = false;
     [HideInInspector] public bool playerCanMove = true; // Used to prevent player movement during interactions
 
     void Awake() //special Unity MonoBehaviour method that is called when the script instance is being loaded
@@ -21,31 +19,64 @@ public class InputManager : MonoBehaviour // MonoBehavior is a base class in Uni
         playerInput = new PlayerInput(); //create a new instance of the pLAYERiNPUT CLASS
         onFoot = playerInput.OnFoot;
 
-        motor = GetComponent<PlayerMotor>(); // fetches the PlayerMotor component attached to the same GameObject, after getting the reference to the PlayerMotor, you can call methods from the PlayerMotor script, such as ProcessMove()
+        motor = GetComponent<PlayerMotor>(); // fetches the PlayerMotor component attached to the same GameObject, after getting the reference tot he PlayerMotor, you cna call methods from the PlayerMotor script, such as ProcessMove()
         look = GetComponent<PlayerLook>();
         onFoot.Jump.performed += ctx => motor.Jump();
 
         onFoot.Crouch.performed += ctx => motor.Crouch();
         onFoot.Sprint.performed += ctx => motor.Sprint();
+
+        
         
     } // the Awake() method initializes the player's input system by creating a new instance o fthe PlayerInput, and then it stores the "OnFoot" action map in the onFoot variable. This onFoot variable contains controls related to player movement.
 
+
+void Start()
+    {
+        // Find the inventory canvas object by tag
+        inventoryCanvas = GameObject.FindGameObjectWithTag("Inventory");
+
+        if (inventoryCanvas != null)
+        {
+            // Initially hide the inventory canvas
+            inventoryCanvas.SetActive(isInventoryVisible);
+        }
+        else
+        {
+            Debug.LogWarning("Inventory canvas not found. Make sure the canvas is tagged 'Inventory'.");
+        }
+
+         // Populate inventory items
+            InventoryManager.Instance.ListItems();
+    }
+
+     void Update()
+    {
+        // Check if "R" key is pressed
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ToggleInventory();
+        }
+    }
     void FixedUpdate()
     {
         if (playerCanMove){
             motor.ProcessMove(onFoot.Movement.ReadValue<Vector2>()); // onFoot contains various input actions related to player movement; ReadValue<Vector2>(): this function reads the current value of the Movement input as a Vector2 which contains two components: x and y.
         }
     }
+    
     // Update is called once per frame
     void LateUpdate()
     {
         if (playerCanMove){
             look.ProcessLook(onFoot.Look.ReadValue<Vector2>());
         }
+
     }
     
     private void OnEnable() // enable the OnFoot action map when GameObject is activated
     {
+        
         onFoot.Enable(); 
     }
 
@@ -53,6 +84,25 @@ public class InputManager : MonoBehaviour // MonoBehavior is a base class in Uni
     {
         onFoot.Disable();
     }
+
+    void ToggleInventory()
+    {
+    // Toggle the inventory visibility
+    isInventoryVisible = !isInventoryVisible;
+    inventoryCanvas.SetActive(isInventoryVisible);
+
+        if (isInventoryVisible)
+        {
+            // Call ListItems to populate the inventory UI when it's visible
+            InventoryManager.Instance.ListItems();
+        }
+        else
+        {
+            // Optionally, clear the item list UI when the inventory is closed
+            foreach (Transform child in InventoryManager.Instance.itemContent)
+            {
+                Destroy(child.gameObject);
+            }
+        }
+    }
 }
-
-
